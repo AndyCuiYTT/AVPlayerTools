8f11342bd2cd648b85c0a8c2478bb4b69c29b87b
AndyCuiYTT <AndyCuiYTT@163.com>
2018-06-16 16:29:21 +0800

    fix -- test

--- AVPlayerTools/AppDelegate.swift
+++ AVPlayerTools/AppDelegate.swift
@@ -25,7 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-       
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
